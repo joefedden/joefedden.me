df9d7c9371a443b03a8c0b16b4298b383abf9e80
Joe Fedden <jfedden@gmail.com>
2016-06-24 09:52:27 -0700

    Linked CSS and JS

--- js/script.js
+++ js/script.js
@@ -1,8 +1,1 @@
-var http = require("http");
-
-http.createServer(function (request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Hello World\n');
-}).listen(80);
-
-console.log("Server running at joefedden.me");
+console.log("Hello World");

7701303f2ef9eee78bb7495fcf41c50a30990770
Joe Fedden <jfedden@gmail.com>
2017-01-20 10:40:11 -0800

    Added Node

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-require('./app/index');
+var express = require('express');
+var app = express();
+var port = 8000;
+
+app.get('/', function (req, res) {
+    res.send('./public/hmtl/index.html');
+})

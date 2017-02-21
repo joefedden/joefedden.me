348c34456dd3b1b6887d47cae877626405fcdbd6
Joe Fedden <jfedden@gmail.com>
2017-02-21 17:16:27 +0000

    added gulp file (still needs editing)

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var express = require('express')
 var app = express()
-var port = 8000
+var port = 8080
 
 app.get('/', function (req, res) {
-  res.send("Hello World");
+  res.send("Test");
 });
 
 app.listen(port, function () {
